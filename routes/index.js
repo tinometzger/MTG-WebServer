b15571f25961dbeee23a0e9fb3ab21d909c8d8f5
tinometzger <tino.metzger97@gmx.de>
2018-11-16 13:09:51 +0100

    added workflow

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'admin',
+    password : 'admin',
+    database: 'mtgcards'
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    connection.connect();
+    connection.query("SELECT * FROM cards;", function (err, result) {
+        if (err) {
+            throw err;
+            console.log(err);
+        } else {
+            console.log("Hat geklappt");
+            console.log(result);
+        }
+    });
+    connection.end();
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
